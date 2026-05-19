6b9ffc409197f8cd9975d1696f299e5ece2afce0
anupampandey01 <pandeyanupam180@gmail.com>
2026-05-19 13:09:59 +0530

    added controller for join as

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -1,6 +1,7 @@
-const contactValidationSchema = require("../utils/validateSchema");
+const {contactValidationSchema, joinAsValidationSchema} = require("../utils/validateSchema");
 const Contact = require("../models/contact");
 const Counselling = require("../models/counselling");
+const Join = require("../models/join");
 
 const saveContactData = async (req, res)=>{
     const { fullName, gender, contactNumber, email } = req.body;
@@ -83,7 +84,47 @@ const saveBookingData = async (req, res)=>{
     }
 }
 
+const saveJoinUs = async (req, res)=>{
+    const { organizationName, joinAs, contactNumber, email } = req.body;
+    const { error } = joinAsValidationSchema.validate(req.body);
+    
+    if (error){
+        return res.status(400).json({
+            success: false,
+            message: error.details[0].message
+        });
+    }
+
+    try {
+        const oldMail = await Join.findOne({email});
+        const oldMobile = await Join.findOne({contactNumber});
+
+        if (oldMobile || oldMail){
+            return res.status(400).json({
+                success: false, 
+                message: "You're in our database. We'll contact you within 24 hrs",
+            })
+        }
+
+        const contact = new Join(req.body);
+        await contact.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Our team will contact you with in 24 hrs",
+            data: contact
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
 module.exports = {
     saveContactData,
     saveBookingData,
+    saveJoinUs
 }
